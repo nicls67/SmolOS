41390f6e96cb9233fb5e9f5957a454c0e6084390
Nicolas SIMON <nicolas.simon6790@gmail.com>
2025-08-27 14:51:00 +0200

    Integrate `hal_interface` dependency and enhance boot configuration with HAL and configurable `systick_period`. Update related modules and add IntelliJ dictionary.

--- libs/kernel/src/lib.rs
+++ libs/kernel/src/lib.rs
@@ -13,15 +13,15 @@ pub use types::*;
 
 pub struct BootConfig {
     pub sched_period: Milliseconds,
+    pub systick_period: Milliseconds,
+    pub hal: HalConfig,
 }
 
 pub fn boot(config: BootConfig) {
     /////////////////////////
     // HAL initialization
     /////////////////////////
-    let hal = Hal::init(HalConfig {
-        core_clk_config: CoreClkConfig::Max,
-    });
+    let hal = Hal::init(config.hal);
 
     /////////////////////////
     // Kernel initialization
@@ -36,13 +36,15 @@ pub fn boot(config: BootConfig) {
     let cortex_p = Kernel::cortex_peripherals();
     cortex_p.SYST.set_clock_source(SystClkSource::Core);
     cortex_p.SYST.clear_current();
-    cortex_p.SYST.set_reload(Kernel::hal().core_clk_freq / 1000);
+    cortex_p
+        .SYST
+        .set_reload(Kernel::hal().core_clk_freq * config.systick_period.to_u32() / 1000);
     cortex_p.SYST.enable_interrupt();
 
     // Initialize scheduler periodic IT
     unsafe {
         cortex_p.SCB.set_priority(SystemHandler::PendSV, 0xFF);
-        set_ticks_target(config.sched_period.to_u32())
+        set_ticks_target(config.sched_period.to_u32() / config.systick_period.to_u32())
     }
 
     // Start Systick
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 
 use cortex_m_rt::entry;
+use hal_interface::{CoreClkConfig, HalConfig};
 use kernel::{BootConfig, Milliseconds};
 
 #[entry]
@@ -9,6 +10,10 @@ fn main() -> ! {
     // Start kernel
     kernel::boot(BootConfig {
         sched_period: Milliseconds(1000),
+        systick_period: Milliseconds(1),
+        hal: HalConfig {
+            core_clk_config: CoreClkConfig::Max,
+        },
     });
 
     //let mut led = Output::new(p.PJ13, Level::High, Speed::Low);
