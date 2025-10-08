a858db2e5154f91d36fdf8b98024f1c16248603e
Nicolas SIMON <nicolas.simon6790@gmail.com>
2025-10-08 09:14:00 +0200

    Expand font support by adding 12px and 16px ASCII fonts
    Replace the existing 8x8 `FONT_ASCII` with `FONT_ASCII_12` (12px) and `FONT_ASCII_16` (16px) data, increasing character resolution and flexibility for rendering.

--- libs/display/src/lib.rs
+++ libs/display/src/lib.rs
@@ -4,11 +4,11 @@ mod errors;
 mod fonts;
 
 pub use errors::{DisplayError, DisplayErrorLevel, DisplayResult};
+pub use fonts::FontSize;
 use hal_interface::{
     Hal, InterfaceReadAction, InterfaceWriteActions, LcdActions, LcdLayer, LcdPixel, LcdReadAction,
 };
 
-use crate::fonts::FONT_ASCII;
 pub use colors::Colors;
 use hal_interface::InterfaceReadResult::LcdRead;
 use hal_interface::LcdRead::LcdSize;
@@ -115,36 +115,31 @@ impl Display {
         x: u16,
         y: u16,
         color: Colors,
-        font_size: u8,
+        font_size: FontSize,
     ) -> DisplayResult<()> {
         if !self.initialized {
             return Err(DisplayError::DisplayDriverNotInitialized);
         }
 
-        for line in 0..8 {
-            for col in 0..8 {
-                let col_data = FONT_ASCII[(ascii_char - 0x20) as usize][line as usize];
-                let pixel_set = col_data & (1 << (7 - col)) != 0;
-                if pixel_set {
-                    for i in 0..font_size {
-                        for j in 0..font_size {
-                            self.hal
-                                .as_mut()
-                                .unwrap()
-                                .interface_write(
-                                    self.hal_id.unwrap(),
-                                    InterfaceWriteActions::Lcd(LcdActions::DrawPixel(
-                                        LcdLayer::FOREGROUND,
-                                        LcdPixel {
-                                            x: x + (col * font_size + i) as u16,
-                                            y: y + (line * font_size + j) as u16,
-                                            color: color.to_argb(),
-                                        },
-                                    )),
-                                )
-                                .map_err(DisplayError::HalError)?;
-                        }
-                    }
+        let char_size = font_size.get_char_size();
+        for line in 0..char_size.1 {
+            for col in 0..char_size.0 {
+                if font_size.is_pixel_set(ascii_char, col, line) {
+                    self.hal
+                        .as_mut()
+                        .unwrap()
+                        .interface_write(
+                            self.hal_id.unwrap(),
+                            InterfaceWriteActions::Lcd(LcdActions::DrawPixel(
+                                LcdLayer::FOREGROUND,
+                                LcdPixel {
+                                    x: x + col as u16,
+                                    y: y + line as u16,
+                                    color: color.to_argb(),
+                                },
+                            )),
+                        )
+                        .map_err(DisplayError::HalError)?;
                 }
             }
         }
